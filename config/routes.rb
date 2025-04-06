c151421d0096eb67f1c3e3f0045037fcc411a2ad
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2025-04-06 12:01:27 +0000

    finished but needs pdate redirect form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :books
   # Routes for the Movie resource:
-
-  # CREATE
+  #New link for movie new  form 
+   get("/movies/new", { :controller => "movies", :action => "new" })
+  
+   get "/movies/:id/edit", controller: "movies", action: "edit"
+  
+   # CREATE
   post("/movies", { :controller => "movies", :action => "create" })
           
   # READ
